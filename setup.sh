538cafe4eccda305fd7a9a52c983b6144ca60287
Prashanna <prashanna.r@idp.com>
2022-08-20 21:11:19 +0530

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,4 +5,5 @@ VENV=${1:-venv}
 python3.8 -m venv $VENV && \
   source $VENV/bin/activate && \
   python3.8 -m pip install --upgrade pip && \
-  pip install -e '.[build]'
+  pip install -e '.[build]' && \
+  pip install openapi_definition/v1/client
